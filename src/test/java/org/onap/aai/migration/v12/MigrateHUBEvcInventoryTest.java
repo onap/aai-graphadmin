072e0e345a354dc201080e5e90aae8b41a4d555c
Fiete Ostkamp <Fiete.Ostkamp@telekom.de>
2024-06-12 14:00:53 +0200

    Update aai-common to 1.14.0 in graphadmin
    - update aai-common to 1.14.0 which contains Janusgraph update to 0.3.3
    - introduce -v parameter to the datasnapshot scripts to specify graphson version
    
    Issue-ID: AAI-3871
    Change-Id: I4efac6b8ef6cf6f5b264677c5a85e38cd226a194
    Signed-off-by: Fiete Ostkamp <Fiete.Ostkamp@telekom.de>

--- src/test/java/org/onap/aai/migration/v12/MigrateHUBEvcInventoryTest.java
+++ src/test/java/org/onap/aai/migration/v12/MigrateHUBEvcInventoryTest.java
@@ -296,7 +296,7 @@ public class MigrateHUBEvcInventoryTest extends AAISetup {
 		TransactionalGraphEngine.Admin adminSpy = spy(dbEngine.asAdmin());
 
 		GraphTraversalSource traversal = g;
-		GraphTraversalSource readOnly = tx.traversal(GraphTraversalSource.build().with(ReadOnlyStrategy.instance()));
+		GraphTraversalSource readOnly = graph.traversal().withStrategies(ReadOnlyStrategy.instance());
 		when (spy.tx()).thenReturn(tx);
 		when(spy.asAdmin()).thenReturn(adminSpy);
 		when(adminSpy.getTraversalSource()).thenReturn(traversal);
